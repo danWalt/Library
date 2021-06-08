35cb1df2bce4f73a491fe1c6899566fea73aa2b0
daniel walters <daniel.walters2110@gmail.com>
2021-06-08 19:08:10 +0300

    added id numbers to book objects

--- public/src/bookObjectFactory.js
+++ public/src/bookObjectFactory.js
@@ -1,13 +1,26 @@
+let id = 0;
+
 const book = {
     init: function(title, author, numberOfPages, readOrNot) {
     this.title = title
     this.author = author
     this.numberOfPages = numberOfPages
     this.readOrNot = readOrNot
+    this.id = generateID()
     this.info = () => {
         return (readOrNot ? this.title + ' by ' + this.author + ', ' + this.numberOfPages + ' pages, read' :
          this.title + ' by ' + this.author + ', ' + this.numberOfPages + ' pages, not read yet')
     }
+
+    // function toggleBook(){
+    //     this.readOrNot ? this.readOrNot = false : this.readOrNot = true
+    // }
+
+
+    function generateID() {
+        return id++
+    }
+        
     return this
 }
 }
